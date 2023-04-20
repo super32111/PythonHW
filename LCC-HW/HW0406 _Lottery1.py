a1659cd70c31dd61676796160f0452051845eb48
super32111 <128707326+super32111@users.noreply.github.com>
2023-04-20 18:18:44 +0800

    Add files via upload
    python homework 0406 for "Lottery" coding creation

---
+++ LCC-HW/HW0406 _Lottery1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 18:32:18 2023
+
+@author: USER
+"""
+
+import random
+min = 1
+max = 49
+
+for i in range(5):
+    ram = random.randint(min,max)
+    print(ram)
+    
+
+
---
+++ LCC-HW/HW0406 _Lottery3.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 20 15:16:16 2023
+
+@author: User
+"""
+
+import random
+
+min = 1
+max = 49
+
+num = []
+while len(num) < 6:
+    n = int(input("請輸入6個號碼(1-49):"))
+    if num.count(n) == 0:
+        num.append(n)
+    else:
+        print("請重新輸入6個號碼(1-49)")
+print(num)
+
+m = []
+for i in range(6):
+    ram = random.randint(min, max)
+    m.append(ram)
+print(m)
+
+yes =[ x for x in num for y in m if x == y]
+if len(yes)>=2:
+    print("中獎",yes)
+else:
+    print("沒中獎")
+
+
+
